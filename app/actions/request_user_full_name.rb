11f6b75838a5a551de560b40740f53c640f9c09d
Ryan Norman <rnorman@covermymeds.com>
2021-01-04 12:16:55 -0500

    Sends response message to collect user information

--- app/actions/request_user_full_name.rb
+++ app/actions/request_user_full_name.rb
@@ -4,7 +4,19 @@ class RequestUserFullName
   end
 
   def execute
-    # request user full name
-    # create event
+    require 'twilio-ruby'
+
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    from = ENV['TWILIO_PHONE_NUMBER']
+    to = @message.from
+
+    client.messages.create(
+      from: from,
+      to: to,
+      body: 'Great to hear you want to get vaccinated! Let\'s collect some information so we can contact you when ready? What is your full name?'
+    )
   end
 end
